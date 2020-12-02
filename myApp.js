2be804399390d5835a960bb936960aceeac65912
texas2010 <9701609+texas2010@users.noreply.github.com>
2020-12-02 00:23:56 -0600

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`)
 })
 
-app.use(`${__dirname}/public`, express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`))
 
 
 
