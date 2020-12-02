3f803d4867f3c8ee0cd49bcdb95231404d23223c
texas2010 <9701609+texas2010@users.noreply.github.com>
2020-12-02 15:04:21 -0600

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -8,11 +8,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/json', (req, res) => {
-    res.json({message: 'Hello json'})
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({message: 'HELLO JSON'})
+    } else {
+        res.json({message: 'Hello json'})
+    }
 })
 
 app.use(express.static(`${__dirname}/public`))
-
-
 
 module.exports = app;
